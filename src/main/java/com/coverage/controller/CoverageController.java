5930f103bef75b38b5a7a1229de62cc80b64326e
anand <anand.bhunia18@gmail.com>
2023-11-03 13:23:29 +0530

    added eureka client and swagger

--- src/main/java/com/coverage/controller/CoverageController.java
+++ src/main/java/com/coverage/controller/CoverageController.java
@@ -15,45 +15,78 @@ import org.springframework.web.bind.annotation.RestController;
 import com.coverage.model.Coverage;
 import com.coverage.service.CoverageService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 public class CoverageController {
-	
+
 	@Autowired
 	private CoverageService coverageService;
-	
-     @PostMapping("/addCoverage")
-	public Coverage createCoverage (@RequestBody Coverage coverage) {
+
+	@PostMapping("/addCoverage")
+	@ApiOperation(value = "Request to add coverage")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Created"), 
+			@ApiResponse(code = 400, message = "Invalid Request"),
+			@ApiResponse(code = 500, message = "Internal Error")})
+	public Coverage createCoverage(@RequestBody Coverage coverage) {
 		Coverage add = coverageService.createCoverage(coverage);
 		return add;
-		
+
 	}
+
 	@PutMapping("/editCoverage")
-	public Coverage updateCoverage (@RequestBody Coverage coverage) {
-		Coverage update= coverageService.updateCoverage(coverage);
+	@ApiOperation(value = "Request to edit coverage")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
+	public Coverage updateCoverage(@RequestBody Coverage coverage) {
+		Coverage update = coverageService.updateCoverage(coverage);
 		return update;
-		
+
 	}
+
 	@DeleteMapping("/deleteCoverage/{id}")
-	public void deleteCoverage (@PathVariable ("id") Integer id) {
+	@ApiOperation(value = "Request to delete coverage by id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
+	public void deleteCoverage(@PathVariable("id") Integer id) {
 		coverageService.deleteCoverage(id);
 	}
+
 	@GetMapping("/getCoverage/{id}")
-	public Coverage getCoverage (@PathVariable ("id") Integer id) {
-		Coverage get= coverageService.getCoverage(id);
+	@ApiOperation(value = "Request to get coverage by id")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
+	public Coverage getCoverage(@PathVariable("id") Integer id) {
+		Coverage get = coverageService.getCoverage(id);
 		return get;
-		
+
 	}
+
 	@GetMapping("/getAllCoverageList")
-	public List<Coverage>  getAllCoverageList(){
-		List<Coverage> getAll= coverageService.getAllCoverageList();
+	@ApiOperation(value = "Request to get all coverage")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
+	public List<Coverage> getAllCoverageList() {
+		List<Coverage> getAll = coverageService.getAllCoverageList();
 		return getAll;
-	
+
 	}
+
 	@GetMapping("/searchByCoverageName")
-	public Coverage searchCoverage (@RequestParam ("coverageName") String coverageName) {
-		Coverage search= coverageService.searchCoverage(coverageName);
+	@ApiOperation(value = "Request to get coverage by coverage name")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Ok"), 
+			@ApiResponse(code = 404, message = "Resource Not Found"),
+			@ApiResponse(code = 500, message = "Internal Error")})
+	public Coverage searchCoverage(@RequestParam("coverageName") String coverageName) {
+		Coverage search = coverageService.searchCoverage(coverageName);
 		return search;
-		
+
 	}
 
 }
